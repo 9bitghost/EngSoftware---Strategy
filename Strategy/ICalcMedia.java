d2c5c6385dc1e9fe3eafb554e2a405b2289a3b33
9bitghost <142262167+9bitghost@users.noreply.github.com>
2024-05-15 12:03:48 -0300

    Update ICalcMedia.java

--- Strategy/ICalcMedia.java
+++ Strategy/ICalcMedia.java
@@ -2,7 +2,7 @@ package Strategy;
 
 public interface ICalcMedia {
 	
-	public double CalculaMedia(double P1, double P2, double media);
+	public double CalculaMedia(double P1, double P2);
 	
 	public String Situacao(double media);
 
