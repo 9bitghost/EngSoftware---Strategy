c3fa599e1d8cde06748e63c82e2c9d30fa34689e
9bitghost <142262167+9bitghost@users.noreply.github.com>
2024-05-15 12:04:36 -0300

    Update Geometrica.java

--- Strategy/Geometrica.java
+++ Strategy/Geometrica.java
@@ -3,18 +3,13 @@ package Strategy;
 public class Geometrica implements ICalcMedia{
 
 	@Override
-	public double CalculaMedia(double P1, double P2, double media) {
-		return media = Math.sqrt(P1 * P2);
+	public double CalculaMedia(double P1, double P2) {
+		return Math.sqrt(P1 * P2);
 	}
 
 	@Override
 	public String Situacao(double media) {
-		if (media > 7) {
-			return "Aprovado";
-		}
-		else {
-			return "Reprovado";
-		}
+		return media > 7 ? "Aprovado" : "Reprovado";
 	}
 
 }
